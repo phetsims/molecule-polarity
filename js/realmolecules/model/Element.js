1494c3da2e51d4bae32c8397ae78653d98d5712b
Chris Malley <cmalley@pixelzoom.com>
2014-02-11 11:25:10 -0700

    flesh out model for 'Real Molecules' screen

---
+++ js/realmolecules/model/Element.js
@@ -0,0 +1,21 @@
+// Copyright 2002-2014, University of Colorado Boulder
+
+/**
+ * Elements, as they are known by Jmol.
+ *
+ * @author Chris Malley (PixelZoom, Inc.)
+ */
+define( function() {
+
+  /**
+   * @param {Number} elementNumber element number in the periodic table
+   * @param {Color|String} color color used by Jmol
+   * @constructor
+   */
+  function Element( elementNumber, color ) {
+    this.elementNumber = elementNumber;
+    this.color = color;
+  }
+
+  return Element;
+} );
