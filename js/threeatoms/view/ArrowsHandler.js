6f21bbc9313a61d9eefd2a4e5cc4a0efa592e9fe
samreid <reids@colorado.edu>
2015-11-06 21:07:16 -0700

    updated copyright info using grunt update-copyright-dates (2nd pass), see  https://github.com/phetsims/chipper/issues/403

--- js/threeatoms/view/ArrowsHandler.js
+++ js/threeatoms/view/ArrowsHandler.js
@@ -1,4 +1,4 @@
-// Copyright 2014, University of Colorado Boulder
+// Copyright 2014-2015, University of Colorado Boulder
 
 /**
  * Drag handler for controlling visibility of arrows that appear around atoms as interactivity cues.
