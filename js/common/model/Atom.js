24483008b50f5768ee6847ff9f9c8a74b204ad4a
Chris Malley <cmalley@pixelzoom.com>
2017-07-18 09:41:34 -0600

    doc

--- js/common/model/Atom.js
+++ js/common/model/Atom.js
@@ -49,7 +49,7 @@ define( function( require ) {
     reset: function() {
       this.electronegativityProperty.reset();
 
-      // do not reset location and partial charge, they will be reset by their parent molecule
+      // Do not reset location and partial charge, they will be reset by their parent molecule.
     }
   } );
 } );
