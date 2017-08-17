c20b72d21947d4d1678fc9136c57de746a2a255d
Chris Malley <cmalley@pixelzoom.com>
2017-08-17 13:57:33 -0600

    master: remove cueing arrows on mouseOver feature, #50

--- js/twoatoms/view/DiatomicMoleculeNode.js
+++ js/twoatoms/view/DiatomicMoleculeNode.js
@@ -10,7 +10,6 @@ define( function( require ) {
   'use strict';
 
   // modules
-  var ArrowsInputListener = require( 'MOLECULE_POLARITY/common/view/ArrowsInputListener' );
   var AtomNode = require( 'MOLECULE_POLARITY/common/view/AtomNode' );
   var BondDipoleNode = require( 'MOLECULE_POLARITY/common/view/BondDipoleNode' );
   var BondNode = require( 'MOLECULE_POLARITY/common/view/BondNode' );
@@ -58,47 +57,16 @@ define( function( require ) {
     var dragHandler = new MoleculeAngleDragHandler( molecule, this );
     this.addInputListener( dragHandler );
 
-    // Input listeners that show arrows on mouse over, disabled by default.
-    var arrowsAInputListener = new ArrowsInputListener( arrowsANode );
-    var arrowsBInputListener = new ArrowsInputListener( arrowsBNode );
-
-    // Arrows around atoms A & B are initially visible.
-    // When the user interacts with any atom, make all arrows disappear, and make them appear on mouse over.
+    // When the user drags any atom or bond, hide the cueing arrows.
     var hideArrows = function() {
-
-      // When the molecule is rotated the user...
       if ( dragHandler.dragging ) {
-
-        // register mouse-over listeners
-        atomANode.addInputListener( arrowsAInputListener );
-        atomBNode.addInputListener( arrowsBInputListener );
-
-        // de-register the observer
-        molecule.angleProperty.unlink( hideArrows );
-
-        // hide the arrows
         arrowsANode.visible = arrowsBNode.visible = false;
       }
     };
     molecule.angleProperty.lazyLink( hideArrows );
 
-    // @private Resets the initial behavior of the arrows.
+    // @private makes the cueing arrows visible
     this.resetArrows = function() {
-
-      // de-register mouse-over listeners
-      if ( atomANode.hasInputListener( arrowsAInputListener ) ) {
-        atomANode.removeInputListener( arrowsAInputListener );
-      }
-      if ( atomBNode.hasInputListener( arrowsBInputListener ) ) {
-        atomBNode.removeInputListener( arrowsBInputListener );
-      }
-
-      // re-register observer for the initial behavior
-      if ( !molecule.angleProperty.hasListener( hideArrows ) ) {
-        molecule.angleProperty.lazyLink( hideArrows );
-      }
-
-      // make the arrows visible
       arrowsANode.visible = arrowsBNode.visible = true;
     };
   }
