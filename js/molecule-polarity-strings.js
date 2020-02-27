4a95c66ac73df0922285f5ccbd55aabc6b780599
samreid <reids@colorado.edu>
2020-02-27 12:07:59 -0700

    Add images and strings, see https://github.com/phetsims/chipper/issues/875

---
+++ js/molecule-polarity-strings.js
@@ -0,0 +1,14 @@
+// Copyright 2020, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+
+import getStringModule from '../../chipper/js/getStringModule.js';
+import moleculePolarity from './moleculePolarity.js';
+
+const moleculePolarityStrings = getStringModule( 'MOLECULE_POLARITY' );
+
+moleculePolarity.register( 'moleculePolarityStrings', moleculePolarityStrings );
+
+export default moleculePolarityStrings;
