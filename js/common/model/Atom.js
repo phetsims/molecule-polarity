18a2fe384b630ff46fbc20209abae42ce2b9672b
pixelzoom <cmalley@pixelzoom.com>
2021-03-08 23:08:29 -0700

    no studio control for electronegativityProperty, #94

--- js/common/model/Atom.js
+++ js/common/model/Atom.js
@@ -51,7 +51,8 @@ class Atom {
     // @public
     this.electronegativityProperty = new NumberProperty( options.electronegativity, {
       range: MPConstants.ELECTRONEGATIVITY_RANGE,
-      tandem: options.tandem.createTandem( 'electronegativityProperty' )
+      tandem: options.tandem.createTandem( 'electronegativityProperty' ),
+      phetioStudioControl: false // because not continuous, see https://github.com/phetsims/molecule-polarity/issues/94
     } );
 
     // @public partial charge is zero until this atom participates in a bond
