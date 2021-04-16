020e9a7a20c1ae42b7f2aa107fcf599c56e63a61
Amy Rouinfar <amy.rouinfar@colorado.edu>
2021-04-16 09:26:39 -0600

    first pass at phetioFeatured for #104

--- js/molecule-polarity-phet-io-overrides.js
+++ js/molecule-polarity-phet-io-overrides.js
@@ -1,2 +1,280 @@
 /* eslint-disable */
-window.phet.preloads.phetio.phetioElementsOverrides = {};
+window.phet.preloads.phetio.phetioElementsOverrides =
+  {
+    "moleculePolarity.global.model.dipoleDirectionProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.threeAtomsScreen.model.eFieldEnabledProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.threeAtomsScreen.model.molecule.atomA.electronegativityProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.threeAtomsScreen.model.molecule.atomA.labelProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.threeAtomsScreen.model.molecule.atomA.partialChargeProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.threeAtomsScreen.model.molecule.atomA.positionProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.threeAtomsScreen.model.molecule.atomB.electronegativityProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.threeAtomsScreen.model.molecule.atomB.labelProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.threeAtomsScreen.model.molecule.atomB.partialChargeProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.threeAtomsScreen.model.molecule.atomB.positionProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.threeAtomsScreen.model.molecule.atomC.electronegativityProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.threeAtomsScreen.model.molecule.atomC.labelProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.threeAtomsScreen.model.molecule.atomC.partialChargeProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.threeAtomsScreen.model.molecule.atomC.positionProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.threeAtomsScreen.model.molecule.bondAB.angleProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.threeAtomsScreen.model.molecule.bondAB.dipoleProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.threeAtomsScreen.model.molecule.bondAngleABCProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.threeAtomsScreen.model.molecule.bondBC.angleProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.threeAtomsScreen.model.molecule.bondBC.dipoleProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.threeAtomsScreen.model.molecule.dipoleProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.threeAtomsScreen.view.controlPanel.eFieldControl.onOffSwitch.visibleProperty": {
+      "phetioFeatured": false
+    },
+    "moleculePolarity.threeAtomsScreen.view.controlPanel.eFieldControl.titleText.textProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.threeAtomsScreen.view.controlPanel.eFieldControl.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.threeAtomsScreen.view.controlPanel.separator1.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.threeAtomsScreen.view.controlPanel.viewControls.bondDipolesCheckbox.labelNode.labelIcon.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.threeAtomsScreen.view.controlPanel.viewControls.bondDipolesCheckbox.labelNode.labelText.textProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.threeAtomsScreen.view.controlPanel.viewControls.molecularDipoleCheckbox.labelNode.labelText.textProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.threeAtomsScreen.view.controlPanel.viewControls.partialChargesCheckbox.labelText.textProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.threeAtomsScreen.view.controlPanel.viewControls.titleText.textProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.threeAtomsScreen.view.controlPanel.viewControls.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.threeAtomsScreen.view.controlPanel.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.threeAtomsScreen.view.electronegativityPanels.atomAElectronegativityPanel.slider.visibleProperty": {
+      "phetioFeatured": false
+    },
+    "moleculePolarity.threeAtomsScreen.view.electronegativityPanels.atomAElectronegativityPanel.subtitleText.textProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.threeAtomsScreen.view.electronegativityPanels.atomAElectronegativityPanel.titleText.textProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.threeAtomsScreen.view.electronegativityPanels.atomAElectronegativityPanel.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.threeAtomsScreen.view.electronegativityPanels.atomBElectronegativityPanel.slider.visibleProperty": {
+      "phetioFeatured": false
+    },
+    "moleculePolarity.threeAtomsScreen.view.electronegativityPanels.atomBElectronegativityPanel.subtitleText.textProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.threeAtomsScreen.view.electronegativityPanels.atomBElectronegativityPanel.titleText.textProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.threeAtomsScreen.view.electronegativityPanels.atomBElectronegativityPanel.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.threeAtomsScreen.view.electronegativityPanels.atomCElectronegativityPanel.slider.visibleProperty": {
+      "phetioFeatured": false
+    },
+    "moleculePolarity.threeAtomsScreen.view.electronegativityPanels.atomCElectronegativityPanel.subtitleText.textProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.threeAtomsScreen.view.electronegativityPanels.atomCElectronegativityPanel.titleText.textProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.threeAtomsScreen.view.electronegativityPanels.atomCElectronegativityPanel.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.threeAtomsScreen.view.electronegativityPanels.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.threeAtomsScreen.view.moleculeNode.hintArrowsNode.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.threeAtomsScreen.view.viewProperties.bondDipolesVisibleProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.threeAtomsScreen.view.viewProperties.molecularDipoleVisibleProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.threeAtomsScreen.view.viewProperties.partialChargesVisibleProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.twoAtomsScreen.model.eFieldEnabledProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.twoAtomsScreen.model.molecule.angleProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.twoAtomsScreen.model.molecule.atomA.electronegativityProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.twoAtomsScreen.model.molecule.atomA.labelProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.twoAtomsScreen.model.molecule.atomA.partialChargeProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.twoAtomsScreen.model.molecule.atomA.positionProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.twoAtomsScreen.model.molecule.atomB.electronegativityProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.twoAtomsScreen.model.molecule.atomB.labelProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.twoAtomsScreen.model.molecule.atomB.partialChargeProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.twoAtomsScreen.model.molecule.atomB.positionProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.twoAtomsScreen.model.molecule.bond.dipoleProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.twoAtomsScreen.model.molecule.dipoleProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.twoAtomsScreen.view.controlPanel.eFieldControl.onOffSwitch.visibleProperty": {
+      "phetioFeatured": false
+    },
+    "moleculePolarity.twoAtomsScreen.view.controlPanel.eFieldControl.titleText.textProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.twoAtomsScreen.view.controlPanel.eFieldControl.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.twoAtomsScreen.view.controlPanel.separator1.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.twoAtomsScreen.view.controlPanel.separator2.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.twoAtomsScreen.view.controlPanel.surfaceControl.radioButtonGroup.electronDensityText.textProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.twoAtomsScreen.view.controlPanel.surfaceControl.radioButtonGroup.electrostaticPotentialText.textProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.twoAtomsScreen.view.controlPanel.surfaceControl.radioButtonGroup.noneText.textProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.twoAtomsScreen.view.controlPanel.surfaceControl.titleText.textProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.twoAtomsScreen.view.controlPanel.surfaceControl.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.twoAtomsScreen.view.controlPanel.viewControls.bondCharacterCheckbox.labelText.textProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.twoAtomsScreen.view.controlPanel.viewControls.bondDipoleCheckbox.labelNode.labelIcon.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.twoAtomsScreen.view.controlPanel.viewControls.bondDipoleCheckbox.labelNode.labelText.textProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.twoAtomsScreen.view.controlPanel.viewControls.partialChargesCheckbox.labelText.textProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.twoAtomsScreen.view.controlPanel.viewControls.titleText.textProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.twoAtomsScreen.view.controlPanel.viewControls.titleText.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.twoAtomsScreen.view.controlPanel.viewControls.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.twoAtomsScreen.view.controlPanel.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.twoAtomsScreen.view.electronegativityPanels.atomAElectronegativityPanel.slider.visibleProperty": {
+      "phetioFeatured": false
+    },
+    "moleculePolarity.twoAtomsScreen.view.electronegativityPanels.atomAElectronegativityPanel.subtitleText.textProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.twoAtomsScreen.view.electronegativityPanels.atomAElectronegativityPanel.titleText.textProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.twoAtomsScreen.view.electronegativityPanels.atomAElectronegativityPanel.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.twoAtomsScreen.view.electronegativityPanels.atomBElectronegativityPanel.slider.visibleProperty": {
+      "phetioFeatured": false
+    },
+    "moleculePolarity.twoAtomsScreen.view.electronegativityPanels.atomBElectronegativityPanel.subtitleText.textProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.twoAtomsScreen.view.electronegativityPanels.atomBElectronegativityPanel.titleText.textProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.twoAtomsScreen.view.electronegativityPanels.atomBElectronegativityPanel.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.twoAtomsScreen.view.electronegativityPanels.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.twoAtomsScreen.view.moleculeNode.hintArrowsNode.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.twoAtomsScreen.view.viewProperties.bondCharacterVisibleProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.twoAtomsScreen.view.viewProperties.bondDipoleVisibleProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.twoAtomsScreen.view.viewProperties.partialChargesVisibleProperty": {
+      "phetioFeatured": true
+    },
+    "moleculePolarity.twoAtomsScreen.view.viewProperties.surfaceTypeProperty": {
+      "phetioFeatured": true
+    }
+  };
