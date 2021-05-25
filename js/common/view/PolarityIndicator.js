ea7e5e8f42ee6c2cd27463f54646d5f792fc6780
phet-dev <phetadmin@gmail.com>
2021-05-25 15:55:41 -0600

    update copyright dates from daily grunt work

--- js/common/view/PolarityIndicator.js
+++ js/common/view/PolarityIndicator.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2020, University of Colorado Boulder
+// Copyright 2014-2021, University of Colorado Boulder
 
 /**
  * Indicator of polarity (positive or negative) used on E-field plates.
