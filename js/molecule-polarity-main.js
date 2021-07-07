9859d9ddd810061cce740f1614dbb05aa769acaf
pixelzoom <cmalley@pixelzoom.com>
2021-07-07 09:48:05 -0600

    phetioDesigned:true, https://github.com/phetsims/molecule-polarity/issues/118

--- js/molecule-polarity-main.js
+++ js/molecule-polarity-main.js
@@ -15,21 +15,6 @@ import RealMoleculesScreen from './realmolecules/RealMoleculesScreen.js';
 import ThreeAtomsScreen from './threeatoms/ThreeAtomsScreen.js';
 import TwoAtomsScreen from './twoatoms/TwoAtomsScreen.js';
 
-const options = {
-
-  // Creates content for the Options dialog
-  createOptionsDialogContent: tandem => new MPOptionsNode( {
-    tandem: tandem
-  } ),
-
-  credits: {
-    leadDesign: 'Kelly Lancaster',
-    softwareDevelopment: 'Chris Malley (PixelZoom, Inc.)',
-    team: 'Julia Chamberlain, Emily B. Moore, Robert Parson, Kathy Perkins, Amy Rouinfar',
-    qualityAssurance: 'Steele Dalton, Alex Dornan, Ethan Johnson, Devon Quispe, Kathryn Woessner'
-  }
-};
-
 simLauncher.launch( () => {
 
   const screens = [
@@ -38,6 +23,23 @@ simLauncher.launch( () => {
     new RealMoleculesScreen( { tandem: Tandem.ROOT.createTandem( 'realMoleculesScreen' ) } )
   ];
 
-  const sim = new Sim( moleculePolarityStrings[ 'molecule-polarity' ].title, screens, options );
+  const sim = new Sim( moleculePolarityStrings[ 'molecule-polarity' ].title, screens, {
+
+    // Creates content for the Options dialog
+    createOptionsDialogContent: tandem => new MPOptionsNode( {
+      tandem: tandem
+    } ),
+
+    credits: {
+      leadDesign: 'Kelly Lancaster',
+      softwareDevelopment: 'Chris Malley (PixelZoom, Inc.)',
+      team: 'Julia Chamberlain, Emily B. Moore, Robert Parson, Kathy Perkins, Amy Rouinfar',
+      qualityAssurance: 'Steele Dalton, Alex Dornan, Ethan Johnson, Devon Quispe, Kathryn Woessner'
+    },
+
+    // phet-io options
+    phetioDesigned: true
+  } );
+
   sim.start();
 } );
