f1c6b3a544c85ef6971401c568982b0563954e2f
Sam Reid <reids@colorado.edu>
2021-11-18 17:43:44 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

---
+++ js/moleculePolarityStrings.ts
@@ -0,0 +1,83 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import moleculePolarity from './moleculePolarity.js';
+
+type StringsType = {
+  'molecule-polarity': {
+    'title': string
+  },
+  'screen': {
+    'twoAtoms': string,
+    'threeAtoms': string,
+    'realMolecules': string
+  },
+  'electricField': string,
+  'on': string,
+  'off': string,
+  'bondCharacter': string,
+  'partialCharges': string,
+  'bondDipole': string,
+  'bondDipoles': string,
+  'molecularDipole': string,
+  'electronegativity': string,
+  'less': string,
+  'more': string,
+  'atomA': string,
+  'atomB': string,
+  'atomC': string,
+  'moreCovalent': string,
+  'moreIonic': string,
+  'pattern': {
+    'atomName': string,
+    'symbolName': string,
+    'dipoleDirection': string
+  },
+  'none': string,
+  'electrostaticPotential': string,
+  'electronDensity': string,
+  'surface': string,
+  'view': string,
+  'atomLabels': string,
+  'molecule': string,
+  'atomElectronegativities': string,
+  'positive': string,
+  'negative': string,
+  'ammonia': string,
+  'borane': string,
+  'boronTrifluoride': string,
+  'carbonDioxide': string,
+  'chloroform': string,
+  'difluoromethane': string,
+  'fluorine': string,
+  'fluoromethane': string,
+  'formaldehyde': string,
+  'hydrogen': string,
+  'hydrogenCyanide': string,
+  'hydrogenFluoride': string,
+  'methane': string,
+  'nitrogen': string,
+  'oxygen': string,
+  'ozone': string,
+  'tetrafluoromethane': string,
+  'trifluoromethane': string,
+  'water': string,
+  'dipoleDirection': string,
+  'deltaPlus': string,
+  'deltaMinus': string,
+  'surfaceColorRealMolecules': string,
+  'underDevelopment': {
+    'line1': string,
+    'line2': string
+  }
+};
+
+const moleculePolarityStrings = getStringModule( 'MOLECULE_POLARITY' ) as StringsType;
+
+moleculePolarity.register( 'moleculePolarityStrings', moleculePolarityStrings );
+
+export default moleculePolarityStrings;
