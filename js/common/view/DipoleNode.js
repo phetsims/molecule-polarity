3652cc38bcd2cf915f224a1716e04a22da2ebd95
zepumph <michael.kauzmann@colorado.edu>
2022-03-04 13:08:07 -0700

    import kite files from imports.ts, https://github.com/phetsims/kite/issues/96

--- js/common/view/DipoleNode.js
+++ js/common/view/DipoleNode.js
@@ -11,7 +11,7 @@
 import Dimension2 from '../../../../dot/js/Dimension2.js';
 import Vector2 from '../../../../dot/js/Vector2.js';
 import Vector2Property from '../../../../dot/js/Vector2Property.js';
-import Shape from '../../../../kite/js/Shape.js';
+import { Shape } from '../../../../kite/js/imports.js';
 import merge from '../../../../phet-core/js/merge.js';
 import AssertUtils from '../../../../phetcommon/js/AssertUtils.js';
 import { Path } from '../../../../scenery/js/imports.js';
