c6d87f5430b1076a1c95f8577b28869283baeaa6
Sam Reid <reids@colorado.edu>
2022-05-07 09:34:41 -0600

    Use semicolons to terminate fields in type StringType, see https://github.com/phetsims/chipper/issues/1208

--- js/moleculePolarityStrings.ts
+++ js/moleculePolarityStrings.ts
@@ -9,70 +9,70 @@ import moleculePolarity from './moleculePolarity.js';
 
 type StringsType = {
   'molecule-polarity': {
-    'title': string
-  },
+    'title': string;
+  };
   'screen': {
-    'twoAtoms': string,
-    'threeAtoms': string,
-    'realMolecules': string
-  },
-  'electricField': string,
-  'on': string,
-  'off': string,
-  'bondCharacter': string,
-  'partialCharges': string,
-  'bondDipole': string,
-  'bondDipoles': string,
-  'molecularDipole': string,
-  'electronegativity': string,
-  'less': string,
-  'more': string,
-  'atomA': string,
-  'atomB': string,
-  'atomC': string,
-  'moreCovalent': string,
-  'moreIonic': string,
+    'twoAtoms': string;
+    'threeAtoms': string;
+    'realMolecules': string;
+  };
+  'electricField': string;
+  'on': string;
+  'off': string;
+  'bondCharacter': string;
+  'partialCharges': string;
+  'bondDipole': string;
+  'bondDipoles': string;
+  'molecularDipole': string;
+  'electronegativity': string;
+  'less': string;
+  'more': string;
+  'atomA': string;
+  'atomB': string;
+  'atomC': string;
+  'moreCovalent': string;
+  'moreIonic': string;
   'pattern': {
-    'atomName': string,
-    'symbolName': string,
-    'dipoleDirection': string
-  },
-  'none': string,
-  'electrostaticPotential': string,
-  'electronDensity': string,
-  'surface': string,
-  'view': string,
-  'atomLabels': string,
-  'molecule': string,
-  'atomElectronegativities': string,
-  'positive': string,
-  'negative': string,
-  'ammonia': string,
-  'borane': string,
-  'boronTrifluoride': string,
-  'carbonDioxide': string,
-  'chloroform': string,
-  'difluoromethane': string,
-  'fluorine': string,
-  'fluoromethane': string,
-  'formaldehyde': string,
-  'hydrogen': string,
-  'hydrogenCyanide': string,
-  'hydrogenFluoride': string,
-  'methane': string,
-  'nitrogen': string,
-  'oxygen': string,
-  'ozone': string,
-  'tetrafluoromethane': string,
-  'trifluoromethane': string,
-  'water': string,
-  'dipoleDirection': string,
-  'deltaPlus': string,
-  'deltaMinus': string,
-  'surfaceColorRealMolecules': string,
+    'atomName': string;
+    'symbolName': string;
+    'dipoleDirection': string;
+  };
+  'none': string;
+  'electrostaticPotential': string;
+  'electronDensity': string;
+  'surface': string;
+  'view': string;
+  'atomLabels': string;
+  'molecule': string;
+  'atomElectronegativities': string;
+  'positive': string;
+  'negative': string;
+  'ammonia': string;
+  'borane': string;
+  'boronTrifluoride': string;
+  'carbonDioxide': string;
+  'chloroform': string;
+  'difluoromethane': string;
+  'fluorine': string;
+  'fluoromethane': string;
+  'formaldehyde': string;
+  'hydrogen': string;
+  'hydrogenCyanide': string;
+  'hydrogenFluoride': string;
+  'methane': string;
+  'nitrogen': string;
+  'oxygen': string;
+  'ozone': string;
+  'tetrafluoromethane': string;
+  'trifluoromethane': string;
+  'water': string;
+  'dipoleDirection': string;
+  'deltaPlus': string;
+  'deltaMinus': string;
+  'surfaceColorRealMolecules': string;
   'underDevelopment': {
-    'line1': string,
-    'line2': string
+    'line1': string;
+    'line2': string;
   }
 };
 
