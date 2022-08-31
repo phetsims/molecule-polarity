55cdead7d8fbc843adca6a3f414d6edd44f8d549
pixelzoom <cmalley@pixelzoom.com>
2022-08-31 09:12:35 -0600

    convert to TS

--- js/common/model/Atom.ts
+++ js/common/model/Atom.ts
@@ -1,6 +1,5 @@
 // Copyright 2014-2022, University of Colorado Boulder
 
-// @ts-nocheck
 /**
  * A make-believe atom whose electronegativity is mutable.
  *
@@ -8,50 +7,62 @@
  */
 
 import NumberProperty from '../../../../axon/js/NumberProperty.js';
+import TProperty from '../../../../axon/js/TProperty.js';
+import TReadOnlyProperty from '../../../../axon/js/TReadOnlyProperty.js';
 import Vector2 from '../../../../dot/js/Vector2.js';
 import Vector2Property from '../../../../dot/js/Vector2Property.js';
-import merge from '../../../../phet-core/js/merge.js';
-import Tandem from '../../../../tandem/js/Tandem.js';
+import optionize from '../../../../phet-core/js/optionize.js';
+import PickRequired from '../../../../phet-core/js/types/PickRequired.js';
+import { TColor } from '../../../../scenery/js/imports.js';
+import { PhetioObjectOptions } from '../../../../tandem/js/PhetioObject.js';
 import moleculePolarity from '../../moleculePolarity.js';
 import MPConstants from '../MPConstants.js';
 
-class Atom {
+type SelfOptions = {
+  diameter: number; // the atom's diameter
+  color: TColor; // base color of the atom
+  position: Vector2; // initial position
+  electronegativity: number;
+};
 
-  /**
-   * @param {TReadOnlyProperty.<string>} labelStringProperty - label that appears on the atom
-   * @param {Object} [options]
-   */
-  constructor( labelStringProperty, options ) {
+export type AtomOptions = SelfOptions & PickRequired<PhetioObjectOptions, 'tandem'>;
 
-    options = merge( {
-      diameter: MPConstants.ATOM_DIAMETER, // {number} the atom's diameter
-      color: 'white', // {Color|string} base color of the atom
-      position: new Vector2( 0, 0 ), // initial position
-      electronegativity: MPConstants.ELECTRONEGATIVITY_RANGE.min, // {number}
-      tandem: Tandem.REQUIRED
-    }, options );
+export default class Atom {
 
-    // @public (read-only)
+  public readonly labelStringProperty: TReadOnlyProperty<string>;
+  public readonly diameter: number;
+  public readonly color: TColor;
+  public readonly positionProperty: TProperty<Vector2>;
+  public readonly electronegativityProperty: NumberProperty;
+  public readonly partialChargeProperty: TProperty<number>;
+
+  public constructor( labelStringProperty: TReadOnlyProperty<string>, providedOptions?: AtomOptions ) {
+
+    const options = optionize<AtomOptions, SelfOptions>()( {
+
+      // SelfOptions
+      diameter: MPConstants.ATOM_DIAMETER,
+      color: 'white',
+      position: new Vector2( 0, 0 ),
+      electronegativity: MPConstants.ELECTRONEGATIVITY_RANGE.min
+    }, providedOptions );
+
+    this.labelStringProperty = labelStringProperty;
     this.diameter = options.diameter;
     this.color = options.color;
 
-    // @public
-    this.labelStringProperty = labelStringProperty;
-
-    // @public
     this.positionProperty = new Vector2Property( options.position, {
       tandem: options.tandem.createTandem( 'positionProperty' ),
       phetioDocumentation: 'The position of this atom. (0,0) is at the upper-LEFT, +x is to the right, and +y is DOWN.',
       phetioReadOnly: true // because position is constrained by molecule structure
     } );
 
-    // @public
     this.electronegativityProperty = new NumberProperty( options.electronegativity, {
       range: MPConstants.ELECTRONEGATIVITY_RANGE,
       tandem: options.tandem.createTandem( 'electronegativityProperty' )
     } );
 
-    // @public partial charge is zero until this atom participates in a bond
+    // partial charge is zero until this atom participates in a bond
     this.partialChargeProperty = new NumberProperty( 0, {
       tandem: options.tandem.createTandem( 'partialChargeProperty' ),
       phetioDocumentation: 'qualitative scalar representation of the partial charge, computed as the electronegativity difference',
@@ -59,14 +70,10 @@ class Atom {
     } );
   }
 
-  // @public
-  reset() {
+  public reset(): void {
     this.electronegativityProperty.reset();
-
-    // Do not reset labelStringProperty.
-    // Do not reset position and partial charge, they will be reset by their parent molecule.
+    // Do not reset positionProperty and partialChargeProperty. They will be reset by their parent molecule.
   }
 }
 
 moleculePolarity.register( 'Atom', Atom );
-export default Atom;
