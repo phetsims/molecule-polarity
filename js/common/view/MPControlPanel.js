65fc9f2fb71b560d2790363e9e460867bff186c9
pixelzoom <cmalley@pixelzoom.com>
2022-08-31 13:16:14 -0600

    // @ts-nocheck

--- js/common/view/MPControlPanel.js
+++ js/common/view/MPControlPanel.js
@@ -1,5 +1,6 @@
 // Copyright 2017-2022, University of Colorado Boulder
 
+// @ts-nocheck
 /**
  * Control panel used throughout Molecule Polarity.
  * Responsible for inserting horizontal spacers between a set of sub-panels.
