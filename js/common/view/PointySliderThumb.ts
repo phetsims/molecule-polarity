25e0d11a990a2d13ad114ac78a108800cac6aae2
Sam Reid <reids@colorado.edu>
2024-04-05 13:46:18 -0600

    Update to TypeScript 5.4.4, see https://github.com/phetsims/chipper/issues/1432

--- js/common/view/PointySliderThumb.ts
+++ js/common/view/PointySliderThumb.ts
@@ -65,7 +65,7 @@ export default class PointySliderThumb extends Path {
       .arc( -0.5 * width + radius, 0.3 * height + heightOffset, radius, Math.PI, Math.PI + angle );
 
     // Save the coordinates for the point above the left side arc, for use on the other side.
-    const sideArcPoint = shape.getLastPoint()!;
+    const sideArcPoint = shape.getLastPoint();
     assert && assert( sideArcPoint );
 
     shape.lineTo( 0, 0 )
