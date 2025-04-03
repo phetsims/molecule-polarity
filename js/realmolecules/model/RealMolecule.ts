563e5119c092a2cd36d2bfc3a7490204fb66d80b
phet-dev <phetadmin@gmail.com>
2025-04-03 03:23:46 -0600

    update copyright dates from daily grunt work

--- js/realmolecules/model/RealMolecule.ts
+++ js/realmolecules/model/RealMolecule.ts
@@ -1,4 +1,4 @@
-// Copyright 2014-2023, University of Colorado Boulder
+// Copyright 2014-2025, University of Colorado Boulder
 
 /**
  * Real molecule that is viewable in PhET's 3D molecule viewer.
