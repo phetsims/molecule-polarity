d7a7df38af226f3335e3e1c11db6f0d757d2ef72
AgustinVallejo <agustin.vallejo@udea.edu.co>
2025-12-15 17:01:29 -0500

    Reordering maps

--- js/common/view/DescriptionMaps.ts
+++ js/common/view/DescriptionMaps.ts
@@ -29,6 +29,112 @@ export default class DescriptionMaps {
     // no-op
   }
 
+  private static deltaENtoPolarity( deltaEN: number ): Polarity {
+    deltaEN = Math.abs( deltaEN );
+    return deltaEN <= 0.05 ? 'nonpolar' :
+           deltaEN <= 0.4 ? 'veryWeaklyPolar' :
+           deltaEN <= 0.8 ? 'weaklyPolar' :
+           deltaEN <= 1.2 ? 'polar' :
+           deltaEN <= 1.6 ? 'stronglyPolar' :
+           'veryStronglyPolar';
+  }
+
+  private static deltaENtoBondDipole( deltaEN: number ): Dipole {
+    deltaEN = Math.abs( deltaEN );
+    return deltaEN <= 0.05 ? 'no' :
+           deltaEN <= 0.4 ? 'verySmall' :
+           deltaEN <= 0.8 ? 'small' :
+           deltaEN <= 1.2 ? 'medium' :
+           deltaEN <= 1.6 ? 'large' :
+           'veryLarge';
+  }
+
+  private static deltaENtoPartialCharges( deltaEN: number ): PartialChargeMagnitude {
+    deltaEN = Math.abs( deltaEN );
+    return deltaEN <= 0.05 ? 'zero' :
+           deltaEN <= 0.4 ? 'verySmall' :
+           deltaEN <= 0.8 ? 'small' :
+           deltaEN <= 1.2 ? 'medium' :
+           deltaEN <= 1.6 ? 'large' :
+           'veryLarge';
+  }
+
+  private static deltaENtoBondCharacter( deltaEN: number ): BondCharacter {
+    deltaEN = Math.abs( deltaEN );
+    return deltaEN <= 0.05 ? 'nonpolarCovalent' :
+           deltaEN <= 0.4 ? 'nearlyNonpolarCovalent' :
+           deltaEN <= 0.8 ? 'slightlyPolarCovalent' :
+           deltaEN <= 1.2 ? 'polarCovalent' :
+           deltaEN <= 1.6 ? 'slightlyIonic' :
+           'mostlyIonic';
+  }
+
+  private static deltaENtoElectrostaticPotential( deltaEN: number ): ElectrostaticPotential {
+    deltaEN = Math.abs( deltaEN );
+    return deltaEN <= 0.05 ? 'noDifference' :
+           deltaEN <= 0.4 ? 'verySmallDifference' :
+           deltaEN <= 0.8 ? 'smallDifference' :
+           deltaEN <= 1.2 ? 'mediumDifference' :
+           deltaEN <= 1.6 ? 'largeDifference' :
+           'veryLargeDifference';
+  }
+
+  private static deltaENtoElectronDensity( deltaEN: number ): ElectronDensity {
+    deltaEN = Math.abs( deltaEN );
+    return deltaEN <= 0.05 ? 'evenlyShared' :
+           deltaEN <= 0.4 ? 'nearlyEvenlyShared' :
+           deltaEN <= 0.8 ? 'slightlyUnevenlyShared' :
+           deltaEN <= 1.2 ? 'unevenlyShared' :
+           deltaEN <= 1.6 ? 'veryUnevenlyShared' :
+           'mostUnevenlyShared';
+  }
+
+  private static deltaENtoElectronDensityShift( deltaEN: number ): ElectronDensityShift {
+    deltaEN = Math.abs( deltaEN );
+    return deltaEN < 0.8 ? 'shiftedSlightly' :
+           deltaEN < 1.2 ? 'shifted' :
+           deltaEN < 1.6 ? 'shiftedMuchMore' :
+           'shiftedAlmostCompletely';
+  }
+
+  private static ENtoQualitative( EN: number ): Electronegativity {
+    return EN === 2.0 ? 'veryLow' :
+           EN < 2.4 ? 'low' :
+           EN < 2.8 ? 'mediumLow' :
+           EN < 3.2 ? 'mediumHigh' :
+           EN < 3.6 ? 'high' :
+           'veryHigh';
+  }
+
+  private static angleToOrientation( angle: number ): Directions {
+    const sin = roundToInterval( Math.sin( angle ), 0.1 );
+    const cos = roundToInterval( Math.cos( angle ), 0.1 );
+
+    if ( sin === 0 ) {
+      return cos > 0 ? 'at3' : 'at9';
+    }
+    else if ( cos === 0 ) {
+      return sin < 0 ? 'at12' : 'at6';
+    }
+    else if ( sin < 0 ) {
+      return cos > 0 ? 'between1And2' : 'between10And11';
+    }
+    else {
+      return cos > 0 ? 'between4And5' : 'between7And8';
+    }
+  }
+
+  private static angleToShape( angle: number ): Shape {
+    const absAngle = Math.abs( angle );
+    return absAngle <= 0.05 * Math.PI ? 'atomsOverlap' :
+           absAngle <= 0.2 * Math.PI ? 'extremelyBentSlightOverlap' :
+           absAngle <= 0.4 * Math.PI ? 'veryBent' :
+           absAngle <= 0.6 * Math.PI ? 'bent' :
+           absAngle <= 0.9 * Math.PI ? 'slightlyBent' :
+           absAngle <= 0.95 * Math.PI ? 'nearlyLinear' :
+           'linear';
+  }
+
   public static createPolarityStringProperty( deltaENProperty: TReadOnlyProperty<number> ): TReadOnlyProperty<string> {
     return MoleculePolarityFluent.a11y.polarity.createProperty( {
       polarity: deltaENProperty.derived( deltaEN => DescriptionMaps.deltaENtoPolarity( deltaEN ) )
@@ -147,112 +253,6 @@ export default class DescriptionMaps {
     return MoleculePolarityFluent.a11y.shape.format( {
       shape: DescriptionMaps.angleToShape( angle )
     } );
-  }
-
-  private static deltaENtoPolarity( deltaEN: number ): Polarity {
-    deltaEN = Math.abs( deltaEN );
-    return deltaEN <= 0.05 ? 'nonpolar' :
-           deltaEN <= 0.4 ? 'veryWeaklyPolar' :
-           deltaEN <= 0.8 ? 'weaklyPolar' :
-           deltaEN <= 1.2 ? 'polar' :
-           deltaEN <= 1.6 ? 'stronglyPolar' :
-           'veryStronglyPolar';
-  }
-
-  private static deltaENtoBondDipole( deltaEN: number ): Dipole {
-    deltaEN = Math.abs( deltaEN );
-    return deltaEN <= 0.05 ? 'no' :
-           deltaEN <= 0.4 ? 'verySmall' :
-           deltaEN <= 0.8 ? 'small' :
-           deltaEN <= 1.2 ? 'medium' :
-           deltaEN <= 1.6 ? 'large' :
-           'veryLarge';
-  }
-
-  private static deltaENtoPartialCharges( deltaEN: number ): PartialChargeMagnitude {
-    deltaEN = Math.abs( deltaEN );
-    return deltaEN <= 0.05 ? 'zero' :
-           deltaEN <= 0.4 ? 'verySmall' :
-           deltaEN <= 0.8 ? 'small' :
-           deltaEN <= 1.2 ? 'medium' :
-           deltaEN <= 1.6 ? 'large' :
-           'veryLarge';
-  }
-
-  private static deltaENtoBondCharacter( deltaEN: number ): BondCharacter {
-    deltaEN = Math.abs( deltaEN );
-    return deltaEN <= 0.05 ? 'nonpolarCovalent' :
-           deltaEN <= 0.4 ? 'nearlyNonpolarCovalent' :
-           deltaEN <= 0.8 ? 'slightlyPolarCovalent' :
-           deltaEN <= 1.2 ? 'polarCovalent' :
-           deltaEN <= 1.6 ? 'slightlyIonic' :
-           'mostlyIonic';
-  }
-
-  private static deltaENtoElectrostaticPotential( deltaEN: number ): ElectrostaticPotential {
-    deltaEN = Math.abs( deltaEN );
-    return deltaEN <= 0.05 ? 'noDifference' :
-           deltaEN <= 0.4 ? 'verySmallDifference' :
-           deltaEN <= 0.8 ? 'smallDifference' :
-           deltaEN <= 1.2 ? 'mediumDifference' :
-           deltaEN <= 1.6 ? 'largeDifference' :
-           'veryLargeDifference';
-  }
-
-  private static deltaENtoElectronDensity( deltaEN: number ): ElectronDensity {
-    deltaEN = Math.abs( deltaEN );
-    return deltaEN <= 0.05 ? 'evenlyShared' :
-           deltaEN <= 0.4 ? 'nearlyEvenlyShared' :
-           deltaEN <= 0.8 ? 'slightlyUnevenlyShared' :
-           deltaEN <= 1.2 ? 'unevenlyShared' :
-           deltaEN <= 1.6 ? 'veryUnevenlyShared' :
-           'mostUnevenlyShared';
-  }
-
-  private static deltaENtoElectronDensityShift( deltaEN: number ): ElectronDensityShift {
-    deltaEN = Math.abs( deltaEN );
-    return deltaEN < 0.8 ? 'shiftedSlightly' :
-           deltaEN < 1.2 ? 'shifted' :
-           deltaEN < 1.6 ? 'shiftedMuchMore' :
-           'shiftedAlmostCompletely';
-  }
-
-  private static ENtoQualitative( EN: number ): Electronegativity {
-    return EN === 2.0 ? 'veryLow' :
-           EN < 2.4 ? 'low' :
-           EN < 2.8 ? 'mediumLow' :
-           EN < 3.2 ? 'mediumHigh' :
-           EN < 3.6 ? 'high' :
-           'veryHigh';
-  }
-
-  private static angleToOrientation( angle: number ): Directions {
-    const sin = roundToInterval( Math.sin( angle ), 0.1 );
-    const cos = roundToInterval( Math.cos( angle ), 0.1 );
-
-    if ( sin === 0 ) {
-      return cos > 0 ? 'at3' : 'at9';
-    }
-    else if ( cos === 0 ) {
-      return sin < 0 ? 'at12' : 'at6';
-    }
-    else if ( sin < 0 ) {
-      return cos > 0 ? 'between1And2' : 'between10And11';
-    }
-    else {
-      return cos > 0 ? 'between4And5' : 'between7And8';
-    }
-  }
-
-  private static angleToShape( angle: number ): Shape {
-    const absAngle = Math.abs( angle );
-    return absAngle <= 0.05 * Math.PI ? 'atomsOverlap' :
-            absAngle <= 0.2 * Math.PI ? 'extremelyBentSlightOverlap' :
-            absAngle <= 0.4 * Math.PI ? 'veryBent' :
-            absAngle <= 0.6 * Math.PI ? 'bent' :
-            absAngle <= 0.9 * Math.PI ? 'slightlyBent' :
-            absAngle <= 0.95 * Math.PI ? 'nearlyLinear' :
-            'linear';
   }
 }
 
