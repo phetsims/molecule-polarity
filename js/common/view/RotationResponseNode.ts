6fe3c3266d4b5ab5bc90f17f76e5e7256487c189
phet-dev <phetadmin@gmail.com>
2026-01-14 03:56:01 -0700

    update copyright dates from daily grunt work

--- js/common/view/RotationResponseNode.ts
+++ js/common/view/RotationResponseNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2025, University of Colorado Boulder
+// Copyright 2026, University of Colorado Boulder
 /**
  * RotationResponseNode handles the context response for a rotating object such as
  * the diatomic molecule for screen 1, or the central atom for screen 3.
