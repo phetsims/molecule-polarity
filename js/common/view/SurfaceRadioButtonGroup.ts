73590792e4a4975d1c391a391af1cf52351fc52c
phet-dev <phetadmin@gmail.com>
2026-01-15 04:17:24 -0700

    update copyright dates from daily grunt work

--- js/common/view/SurfaceRadioButtonGroup.ts
+++ js/common/view/SurfaceRadioButtonGroup.ts
@@ -1,4 +1,4 @@
-// Copyright 2021-2025, University of Colorado Boulder
+// Copyright 2021-2026, University of Colorado Boulder
 
 /**
  * SurfaceRadioButtonGroup is the radio button group for choosing a surface for the molecule.
