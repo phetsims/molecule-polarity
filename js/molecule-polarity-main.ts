8c4a5356b035996cdd00343c6287d63dc1dfc7d5
AgustinVallejo <agustin.vallejo@udea.edu.co>
2026-03-30 12:12:23 -0300

    Updating credits for Molecule Polarity, see https://github.com/phetsims/molecule-polarity/issues/337
    (cherry picked from commit bbb6d2967c2be194696f86469554a39d3de73dcf)

--- js/molecule-polarity-main.ts
+++ js/molecule-polarity-main.ts
@@ -42,8 +42,8 @@ simLauncher.launch( () => {
     credits: {
       leadDesign: 'Kelly Lancaster',
       softwareDevelopment: 'Chris Malley (PixelZoom, Inc.), Jonathan Olson, Agust\u00edn Vallejo',
-      team: 'Julia Chamberlain, Emily B. Moore, Robert Parson, Kathy Perkins, Amy Rouinfar, Nancy Salpepi',
-      qualityAssurance: 'Steele Dalton, Alex Dornan, Jaron Droder, Clifford Hardin, Ethan Johnson, Brooklyn Lash, Emily Miller, Devon Quispe, Nancy Salpepi, Kathryn Woessner'
+      team: 'Julia Chamberlain, Emily B. Moore, Matthew Moore, Robert Parson, Kathy Perkins, Amy Rouinfar, Nancy Salpepi',
+      qualityAssurance: 'Steele Dalton, Alex Dornan, Jaron Droder, Clifford Hardin, Ethan Johnson, Brooklyn Lash, Emily Miller, Valentina P\u00e9rez, Devon Quispe, Kathryn Woessner'
     },
 
     // phet-io options
